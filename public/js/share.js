b697a70fd3de3c9d7c6e27712c4e05adc4a97f06
yogesh <50631450+hound77@users.noreply.github.com>
2021-08-02 23:23:42 +0530

    Update share.js

--- public/js/share.js
+++ public/js/share.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
 //const baseURL = "https://innshare.herokuapp.com";
-const baseURL = "http://localhost:3000"
+const baseURL = "https://file-share7.herokuapp.com"
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
